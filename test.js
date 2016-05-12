802e47d51d8ed33ce77908ddeba25c3cf5ac0f7b
jonschlinkert <jon.schlinkert@sellside.com>
2016-05-12 11:10:42 -0400

    run update

--- test.js
+++ test.js
@@ -1,25 +1,27 @@
+'use strict';
+
 require('mocha');
 var assert = require('assert');
 var name = require('./');
 
-describe('project-name', function () {
-  it('should return the name from package.json', function () {
+describe('project-name', function() {
+  it('should return the name from package.json', function() {
     assert.equal(name(), 'project-name');
   });
 
-  it('should return the name from a filepath', function () {
+  it('should return the name from a filepath', function() {
     assert.equal(name('fixtures/foo/a.txt'), 'foo');
   });
 
-  it('should return the name from a directory', function () {
+  it('should return the name from a directory', function() {
     assert.equal(name('fixtures/foo'), 'foo');
   });
 
-  it('should return the name from an empty directory', function () {
+  it('should return the name from an empty directory', function() {
     assert.equal(name('fixtures/foo/bar'), 'bar');
   });
 
-  it('should return the name from a git config', function () {
+  it('should return the name from a git config', function() {
     assert.equal(name('fixtures/foo'), 'foo');
   });
 });
